258e7779d35ddc054bfdca77b402e21da3e79ae2
barksanson <barksansondev@gmail.com>
2023-12-20 21:20:05 +0100

    Don't remember the changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,13 @@ import { FlightRepository } from './repositories/flights/FlightRepository';
 import {jugadores } from './repositories/jugadores/jugadores';
 import { get_db_pool } from './db/db'; 
 
-const pool = get_db_pool();
+const main = async () => {
+    const pool = await get_db_pool();
+    const flight_repo = new FlightRepository(pool);
+    const jugadores_repo = new jugadores(pool);
+    const controller = new Controller(jugadores_repo,flight_repo);
+    const server = new Server(controller);
+    server.listen();
+}
 
-const flight_repo = new FlightRepository(pool);
-const jugadores_repo = new jugadores(pool);
-
-const controller = new Controller(jugadores_repo,flight_repo);
-
-const server = new Server(controller);
-server.listen();
+main();
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,47 @@ resource "azurerm_resource_group" "asi" {
     location = "eastus"
 }
 
+data "azuread_client_config" "current" {}
+
+resource "azurerm_key_vault" "asi_vault" {
+    name = "asi-vault"
+    resource_group_name = azurerm_resource_group.asi.name
+    location = azurerm_resource_group.asi.location
+    tenant_id = data.azuread_client_config.current.tenant_id
+
+    sku_name = "standard"
+
+    network_acls {
+        default_action = "Deny"
+        bypass = "AzureServices"
+    }
+
+    access_policy {
+        tenant_id = data.azuread_client_config.current.tenant_id
+        object_id = data.azuread_client_config.current.object_id
+
+        key_permissions = [ 
+            "Get",
+            "List",
+            "Sign"
+        ]
+
+        secret_permissions = [
+            "Get",
+            "List",
+            "Set"
+        ] 
+        
+        storage_permissions = [
+            "Get",
+            "List",
+            "RegenerateKey",
+            "Set",
+            "Update"
+        ]
+    }
+}
+
 resource "azurerm_mysql_flexible_server" "asi_data" {
     name                    = "asi-data"
     resource_group_name     = azurerm_resource_group.asi.name
