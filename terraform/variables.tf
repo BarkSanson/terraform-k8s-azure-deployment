39abffbb81db1f795e182603fb1c74e27c181963
Bark Sanson <barksansondev@gmail.com>
2024-01-08 19:15:31 +0100

    Removed client_id and client_secret

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
-variable "client_id" {
-    description = "The Client ID of the Service Principal."
-}
-
-variable "client_secret" {
-    description = "The Client Secret of the Service Principal."
-}
-
 variable "admin_login" {
     description = "The administrator login name for the MySQL Flexible Server."
 }
